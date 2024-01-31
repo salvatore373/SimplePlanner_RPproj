791489ca152ff68304f352b927bc24dc413b9d87
Salvatore Rago <salvatorerago373@gmail.com>
2024-01-31 13:08:20 +0100

    Created project

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/rob_prog/simple_planner/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD='/rob_prog/simple_planner/catkin_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/rob_prog/simple_planner/catkin_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/rob_prog/simple_planner/catkin_ws/src:$ROS_PACKAGE_PATH"
